6b88a395b6b16243b3fe5699008fc6bdb072fedb
Abhimoy-Mukherjee <abhimoymukherjee@gmail.com>
2026-08-19 10:10:46 +0530

    Remove hardcoded jobD and make it read from a file

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from read import read_pdf, read_docx
 from parse_resume import parse_resume, final_score
 from job_description import analyze_job_description
 
-job = analyze_job_description()
+jd_path=Path("job_description.txt")
+job_description_text = jd_path.read_text(encoding="utf-8")
+job = analyze_job_description(job_description_text)
 resume_folder = Path("resumes")
 all_results=[]
 for file_path in resume_folder.iterdir():
-    #C:\Users\Pratyush\padho_with_pratyush\week1\day5\resumes\abhay resume new - Abhay Singh.pdf
     if file_path.suffix.lower() not in [".pdf", ".docx"]:
         continue
     print("\nProcessing:", file_path.name)
@@ -17,13 +18,9 @@ for file_path in resume_folder.iterdir():
         resume_text = read_pdf(file_path)
     elif file_path.suffix.lower() == ".docx":
         resume_text = read_docx(file_path)
-    parsed_resume=parse_resume(resume_text) # llm call1
+    parsed_resume=parse_resume(resume_text)
     time.sleep(3)
-    result = final_score(job, parsed_resume) #llm caLL2
-    #score and details
-    #acount chtgpt
-    # request bhejna shhur krega millions
-    #chattgot server jam ho jayega
+    result = final_score(job, parsed_resume)
     time.sleep(3)
     print("Score:", result.score)
     all_results.append({

48b125c759df91bccd6ece999718070a68c2116f
Abhimoy-Mukherjee <abhimoymukherjee@gmail.com>
2026-08-19 09:28:15 +0530

    Added main function to parse and score resume againts a jobD

--- main.py
+++ main.py
@@ -1,6 +1,63 @@
-def main():
-    print("Hello from resume-parser!")
+import os
+from pathlib import Path
+import time
+from read import read_pdf, read_docx
+from parse_resume import parse_resume, final_score
+from job_description import analyze_job_description
+
+job = analyze_job_description()
+resume_folder = Path("resumes")
+all_results=[]
+for file_path in resume_folder.iterdir():
+    #C:\Users\Pratyush\padho_with_pratyush\week1\day5\resumes\abhay resume new - Abhay Singh.pdf
+    if file_path.suffix.lower() not in [".pdf", ".docx"]:
+        continue
+    print("\nProcessing:", file_path.name)
+    if file_path.suffix.lower() == ".pdf":
+        resume_text = read_pdf(file_path)
+    elif file_path.suffix.lower() == ".docx":
+        resume_text = read_docx(file_path)
+    parsed_resume=parse_resume(resume_text) # llm call1
+    time.sleep(3)
+    result = final_score(job, parsed_resume) #llm caLL2
+    #score and details
+    #acount chtgpt
+    # request bhejna shhur krega millions
+    #chattgot server jam ho jayega
+    time.sleep(3)
+    print("Score:", result.score)
+    all_results.append({
+        "name": parsed_resume.name,
+        "score": result.score,
+        "details": result.details
+    })
+all_results.sort(
+    key=lambda candidate: candidate["score"],
+    reverse=True
+)
+top_2 = all_results[:2]
+worst_2 = all_results[-2:]
 
 
-if __name__ == "__main__":
-    main()
+print("TOP 2 CANDIDATES")
+for candidate in top_2:
+
+    print(
+        candidate["name"],
+        "-",
+        candidate["score"],
+        "%"
+    )
+
+    print(candidate["details"])
+
+print("LOWEST 2 CANDIDATES")
+for candidate in worst_2:
+
+    print(
+        candidate["name"],
+        "-",
+        candidate["score"],
+        "%"
+    )
+    print(candidate["details"])
